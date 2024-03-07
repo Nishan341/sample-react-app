501d7f7355ecad42d077f2049daa3ee05d1b24dd
Nishan341 <160533264+Nishan341@users.noreply.github.com>
2024-03-07 12:42:50 +0000

    updated url

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://expert-zebra-jj9wv64gg47cqpvx-3000.app.github.dev/api/weather/london";
+const API_URL = "https://expert-zebra-jj9wv64gg47cqpvx-3000.app.github.dev/api";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
