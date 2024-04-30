84ea8a791b5d2ba883ac9ada7540dc1ecd715051
Nishan341 <160533264+Nishan341@users.noreply.github.com>
2024-04-30 19:27:20 +0000

    adding sesmic api

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -29,5 +29,37 @@ export const getWeatherData = async (city: string): Promise<WeatherData> => {
           reject("An unknown error occurred");
         }
       });
+      const API_URL ="https://zany-acorn-jjrjvqwpv65x25rq5-3000.app.github.dev/api";
+
+  export const getSesmicData = async (city: string): Promise<SesmicData> => {
+    return new Promise<SesmicData>((resolve, reject) => {
+      axios
+        .get(`${API_URL}/sesmic/${city}`)
+        .then((res) => {
+          resolve({
+            city: city,
+            magnitude: res.data.magnitude,
+            latitude: res.data.latitude,
+            longtitude: res.data.longtitude,
+            
+          });
+        })
+        .catch((error) => {
+          if (axios.isAxiosError(error)) {
+            const axiosError = error as AxiosError;
+            if (axiosError.response?.status === 404) {
+              reject("City not found");
+            } else {
+              // It's a good practice to reject with an Error object
+              reject(axiosError.message);
+            }
+          } else {
+            // Handle non-Axios errors
+            reject("An unknown error occurred");
+          }
+        });
+    });
   });
+  
+
 };
